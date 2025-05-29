8b50ef6be55b3856c0db13beba90bf552fbea9e0
Illia Liashko <2275040@gmail.com>
2025-05-30 00:05:07 +0300

    finish hw

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises } from 'fs';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const content = await promises.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(content);
+
+    return contacts;
+  } catch {
+    console.error('Failed to read file.');
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { promises } from 'fs';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const updatedFileContent = JSON.stringify(updatedContacts);
+  
+  try {
+    await promises.writeFile(PATH_DB, updatedFileContent, 'utf-8');
+  } catch {
+    console.error('Failed to write file');
+  }
+};
